1ceb35ae93f556f8ec55b6f2eafe9bab635855a7
JP Lopez <114964643+jplopez19@users.noreply.github.com>
2023-08-10 15:50:33 -0700

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,21 @@
+# tests/# tests/test_main.py
+
+import pytest
+from your_project_path import main
+from skimage.io import imread_collection
+
+def test_load_images():
+    # Test the load_images function
+    image_path = 'tests'  # Path to the 'tests' folder with sample images
+    images = main.load_images(image_path)
+    assert isinstance(images, imread_collection.ImageCollection)
+    assert len(images) > 0
+
+def test_main():
+    # Test the main function (integration test)
+    # This test assumes that the 'tests' folder contains sample images for testing
+    main.main()
+    # Add assertions to check the results, such as output files, segmented images, etc.
+    # The specific assertions will depend on the expected outcomes of the main function
+
+# Add more test cases for other functions or workflows in main.py if needed
