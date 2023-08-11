1705e5d89f071027523c5e5f5c55d755acd1f214
JP Lopez <114964643+jplopez19@users.noreply.github.com>
2023-08-10 18:11:25 -0700

    Update test_visualization.py

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,14 +1,12 @@
-# tests/test_visualization.py
-
 import pytest
-from your_project_path import visualization
+from microglia_analysis import visualizations  # Update the import path
 import numpy as np
 
 def test_create_coloring_book():
     # Test the create_coloring_book function
     segmented_images = [np.random.rand(100, 100) for _ in range(5)]
     # Call the function (implementation is a TODO in your code)
-    visualization.create_coloring_book(segmented_images)
+    visualizations.create_coloring_book(segmented_images)
     # Add assertions to check the result if needed
 
 def test_plot_thresholding_results():
@@ -16,14 +14,7 @@ def test_plot_thresholding_results():
     image = np.random.rand(100, 100)
     thresholds = {'otsu': np.random.rand(100, 100), 'li': np.random.rand(100, 100)}
     # Call the function
-    visualization.plot_thresholding_results(image, thresholds)
+    visualizations.plot_thresholding_results(image, thresholds)
     # Add assertions to check the result if needed
 
-def test_plot_vampire_results():
-    # Test the plot_vampire_results function
-    vampire_results = {'result1': 'value1', 'result2': 'value2'}
-    # Call the function (implementation is a TODO in your code)
-    visualization.plot_vampire_results(vampire_results)
-    # Add assertions to check the result if needed
-
-# Add more test cases for other functions in visualization.py if needed
+# Add more test cases for other functions in visualizations.py if needed
